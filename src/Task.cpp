c6701c694b445aefbca8e7793b9fd0a7ddd91e60
J. Anthony Sterrett, Jr <jasterrett@gmail.com>
2015-02-26 12:38:54 -0800

    Removed directory logic

--- src/Task.cpp
+++ src/Task.cpp
@@ -87,66 +87,8 @@ void Task::jump(int state) const {
 
 /**
  * Gets the next filename.
- *
- * Half the time, this returns the parent directory (if it exists, and is
- * viable). The other half, it chooses a viable subdirectory (if one exists).
- *
- * If none of the above options are viable, this returns the HOME directory.
- * 
- * @return [description]
  */
 std::string Task::getNext() const {
 
-  const static char FILE_SEP = '/';
-
-  // Copy prgName to char buffer because dirname isn't guaranteed not to modify
-  // its parameter
-  char absPath[prgName.length() + 1];
-  strcpy(absPath, prgName.c_str());
-  absPath[prgName.length()] = '\0';
-
-  std::string nextDir;
-
-  const auto getParentDir = [&absPath] () -> std::string {
-
-    auto dir = dirname(absPath);
-
-    if (!isRWX(dir)) return {};
-    if (dir == absPath) return {};
-
-    return dir;
-  };
-
-  const auto getSubDir = [&absPath, this] () -> std::string {
-
-    auto dirs = getSubdirs(absPath);
-
-    if (dirs.empty()) return {};
-
-    auto dir = *choose(std::begin(dirs), std::end(dirs), g);
-
-    if (!isRWX(dir)) return {};
-
-    return dir;
-  };
-
-  if (std::uniform_int_distribution<int>(0, 1)(g)) {
-
-    nextDir = getParentDir();
-
-    if (nextDir.empty()) nextDir = getSubDir();
-  } else { 
-
-    nextDir = getSubDir();
-
-    if (nextDir.empty()) nextDir = getParentDir();
-  }
-
-  if (nextDir.empty()) nextDir = getHomeDir();
-
-  nextDir += FILE_SEP;
-
-  const auto maxBaseName = PATH_MAX - nextDir.length();
-
-  return nextDir + randomFilename(maxBaseName, g);
+  return randomFilename(g);
 }
